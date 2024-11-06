b14f3ff7d9542aecbbf8ad86198f7550db79bab0
rsvprzdz <rsvprzdz@naver.com>
2024-11-06 14:31:09 +0900

    2024 11-06 김인창 post 컨트롤러,mapper,서비스 부분 및 post html,css,js 파일명 변경 및 수정 완료

--- damgarak/src/main/java/com/kh/damgarak/post/model/vo/Post.java
+++ damgarak/src/main/java/com/kh/damgarak/post/model/vo/Post.java
@@ -1,20 +1,17 @@
-package com.kh.damgarak.board.model.vo;
+package com.kh.damgarak.post.model.vo;
 
 import java.sql.Date;
 import lombok.Data;
 
 @Data
-public class board {
+public class Post {
 	private int post_No;
 	private int post_Count;
 	private String title;
 	private String content;
 	private Date creation_Date;
-	private Date Modification_Date;
+	private Date modification_Date;
 	private String delete_State;
 	private String user_Id;
 	private String category_Code;
-	private String reply_Comment;
-	private String post_Category_Code;
-	private String post_Category_Name;
 }
