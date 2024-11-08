b1f09df7418a6a0a903c1ea535d6541d0c14319d
whdrns456 <whdrns456@naver.com>
2024-11-08 09:24:30 +0900

    2024-11-08 최종군 테이블 좌석예약 예약 기능 작업하고 있습니다

---
+++ damgarak/src/main/java/com/kh/damgarak/tableReservation/model/vo/TableReservation.java
@@ -0,0 +1,13 @@
+package com.kh.damgarak.tableReservation.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TableReservation {
+	
+	private int reservationNumber;
+	private int tableNo;
+	
+}
