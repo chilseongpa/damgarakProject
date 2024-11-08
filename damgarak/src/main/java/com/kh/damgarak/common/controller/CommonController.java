1e6c89fabf8dc940c9d927fcd3c3b0a8d1a95e78
jogunwoong <jgw990928@gmail.com>
2024-11-08 17:49:57 +0900

    2024-11-08 조건웅 선생님 피드백 90%

--- damgarak/src/main/java/com/kh/damgarak/common/controller/CommonController.java
+++ damgarak/src/main/java/com/kh/damgarak/common/controller/CommonController.java
@@ -1,5 +1,18 @@
 package com.kh.damgarak.common.controller;
 
-public class CommonController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.kh.damgarak.post.service.ManagerService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class CommonController {
+	
+	@GetMapping("/map")
+	public String mapPage() {
+		return "damgarak/map";
+	}
 }
