1e4eee4cd26d83b4103dc4a6273b057c90307252
rsvprzdz <rsvprzdz@naver.com>
2024-11-12 09:11:44 +0900

    2024-11-12 김인창 공지사항 조회시 A태그 클릭시  상세 페이지 들어가지는 기능 구현 완료

--- damgarak/src/main/java/com/kh/damgarak/post/model/vo/Notice.java
+++ damgarak/src/main/java/com/kh/damgarak/post/model/vo/Notice.java
@@ -1,11 +1,14 @@
 package com.kh.damgarak.post.model.vo;
 
+import java.sql.Date;
+
 import lombok.Data;
 
 @Data
 public class Notice {
 	private String UsersId;
-	private String NoticeTitle;
-	private String NoticeContent;
-	private int NoticeNo;
+	private String noticeTitle;
+	private String noticeContent;
+	private Date creationDate;
+	private int noticeNo;
 }
