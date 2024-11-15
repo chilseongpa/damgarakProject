cf33ad2ad61d6f0ee04ccb14c41f4e7adf505de8
mindnr0806 <alsdnr6672@naver.com>
2024-11-15 19:34:25 +0900

    2024년 11월 15일 양민욱 도시락 주문하기 기능 구현 - 누락 파일 추가 OrderHistory 클래스

---
+++ damgarak/src/main/java/com/kh/damgarak/order/model/vo/OrderHistory.java
@@ -0,0 +1,10 @@
+package com.kh.damgarak.order.model.vo;
+
+import lombok.Data;
+
+@Data
+public class OrderHistory {
+	private int orderNo;
+	private int reservationNo;
+	private String orderDate;
+}
