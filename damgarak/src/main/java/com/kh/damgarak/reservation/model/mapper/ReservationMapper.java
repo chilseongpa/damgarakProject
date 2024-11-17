ec8a9aed7b542c219c1894ecc41f4112e110b8a8
whdrns456 <whdrns456@naver.com>
2024-11-17 19:50:19 +0900

    2024-11-17 최종군 페이징네이션 및 푸터헤더 적용 및 css 수정과 회원가입 시 직원 선택 시 직원 관련 쿼리문 작성

--- damgarak/src/main/java/com/kh/damgarak/reservation/model/mapper/ReservationMapper.java
+++ damgarak/src/main/java/com/kh/damgarak/reservation/model/mapper/ReservationMapper.java
@@ -8,7 +8,9 @@ import org.apache.ibatis.annotations.Mapper;
 import com.kh.damgarak.reservation.pastReservationSearch.model.dto.PastReservationSearch;
 @Mapper
 public interface ReservationMapper {
-	List<PastReservationSearch> searTableList(HashMap<String, String> map);
-	List<PastReservationSearch> searchLuchList(HashMap<String, String> map);
+	List<PastReservationSearch> searTableList(HashMap<String, String> map, int offset, int limit);
+	List<PastReservationSearch> searchLuchList(HashMap<String, String> map, int offset, int limit);
+	int getCountTable(String usersId);
+	int getCountLunchBox(String usersId);
 }
 
