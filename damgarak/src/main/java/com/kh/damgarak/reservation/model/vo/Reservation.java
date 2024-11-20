7eaa55aa2bc8d5235d3825a312563370a2bbc593
whdrns456 <whdrns456@naver.com>
2024-11-20 17:57:52 +0900

    20204-11-20 최종군 페이지 오류 관해서 커밋 올립니다

--- damgarak/src/main/java/com/kh/damgarak/reservation/model/vo/Reservation.java
+++ damgarak/src/main/java/com/kh/damgarak/reservation/model/vo/Reservation.java
@@ -1,6 +1,5 @@
 package com.kh.damgarak.reservation.model.vo;
 
-import java.sql.Date;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,11 +8,9 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Reservation {
-
 	private String reservationNo;
 	private String usersId;
-	private Date reservationDate;
+	private String reservationDate;
 	private String reservationStatus;
 }
